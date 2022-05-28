08688ee02919f947bc9c4b0d5d75654a4cc8f13c
MuhammadShehzad <mshehzadmshehzad113440@gmail.com>
2022-05-28 17:59:06 +0500

    file reading

--- src/os-kernel.cpp
+++ src/os-kernel.cpp
@@ -1,13 +1,36 @@
+#include<iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <pthread.h>
+#include <string>
+#include<fstream>
+#include "./os-kernel.h"
 
-int main()
+using namespace std;
+int main(int argc, char *argv[])
 {
-    printf("\n\t\tSparkKernel\n");
 
+    string inputfile = argv[1],outputfile = argv[5],ch = argv[3];
+    
+    char algo_name = ch[0];
+    int cpus = stoi(argv[2]);
+    int timeslice = stoi(argv[4]);
+     
+   // cout<<"\n"<<inputfile<<"\n"<<cpus<<"\n"<<algo_name<<"\n"<<timeslice<<"\n"<<outputfile<<"\n";
+    
+   // file reading 
+   ifstream file(inputfile);
+   string line="";
+   getline(file,line);
+
+   while(getline(file,line))
+   {
+       cout<<line<<"\n";
+   }
+    
+    
     return 0;
 }
--- src/os-kernel.h
+++ src/os-kernel.h
@@ -1,8 +1,10 @@
+#include<string>
+using namespace std;
 class Process
 {
 public:
     // 5 states of process
-    // 0   -1      2           3         4
+    //  1       2        3       4          5
     // NEW - READY - RUNNING - WAITING - TERMINATED
     int P_state;
     // process id
@@ -23,7 +25,23 @@ class PCB
 private:
     /* data */
 public:
-    PCB(/* args */);
+    int process_id;
+    string process_name;
+    int process_priority;
+    int process_arival_time;
+    char process_type;
+    int process_cpu_time;
+    int process_IO_time;
+    PCB(int id,string name,int priority,int a_time,char type,int cpu_time,int io_time)
+    {
+        process_id=id;
+        process_name=name;
+        process_priority=priority;
+        process_arival_time=a_time;
+        process_type=type;
+        process_cpu_time=cpu_time;
+        process_IO_time=io_time;
+    }
 };
 
 class SparkKernel
