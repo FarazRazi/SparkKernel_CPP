2dfb01bca0d4fadc8d5b189e2d837e0e71053b54
farazrazi <farazrazi432@gmail.com>
2022-05-30 15:19:02 -0400

    Fixed ProcessShow Row Alignmnet

--- src/os-kernel.cpp
+++ src/os-kernel.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,52 +7,41 @@
 #include <pthread.h>
 #include <string>
 #include <string.h>
-#include<fstream>
+#include <fstream>
 #include "./os-kernel.h"
 
 using namespace std;
 
-
-int total_cpus=0,timeslice=0;
+int total_cpus = 0, timeslice = 0;
 
 int *cpus_array;
 
-
-
-void* start(void *argc)
+void *start(void *argc)
 {
-      
-      pthread_exit(0);
+
+    pthread_exit(0);
 }
 int main(int argc, char *argv[])
 {
-
-    string inputfile = argv[1],outputfile = argv[5],ch = argv[3]; 
+    string inputfile = argv[1], outputfile = argv[5], ch = argv[3];
     char algo_name = ch[0];
     total_cpus = stoi(argv[2]);
     timeslice = stoi(argv[4]);
-   
+
     SparkKernel Kernel;
     Kernel.file_handling(inputfile);
     Kernel.show_pcb();
-
-
-
-
-    pthread_t tid_array[total_cpus];
-    cpus_array= new int[total_cpus];
     
-    for(int i=0;i<total_cpus;i++)
+    pthread_t tid_array[total_cpus];
+    cpus_array = new int[total_cpus];
+
+    for (int i = 0; i < total_cpus; i++)
     {
-        int *j= new int;
-        *j=i+1;
-        pthread_create(&tid_array[i],NULL,start,&j);
+        int *j = new int;
+        *j = i + 1;
+        pthread_create(&tid_array[i], NULL, start, &j);
         pthread_detach(tid_array[i]);
     }
-   
-  
-   
- 
-    
+
     return 0;
 }
--- src/os-kernel.h
+++ src/os-kernel.h
@@ -1,5 +1,5 @@
-#include<string>
-#include<string.h>
+#include <string>
+#include <string.h>
 using namespace std;
 class Process
 {
@@ -36,24 +36,24 @@ public:
     int process_cpu_time;
     int process_IO_time;
 
-    PCB ()
+    PCB()
     {
-        process_id=0;
-        process_name="";
-        process_priority=0;
-        process_arival_time=0;
-        process_cpu_time=0;
-        process_IO_time=0;
+        process_id = 0;
+        process_name = "";
+        process_priority = 0;
+        process_arival_time = 0;
+        process_cpu_time = 0;
+        process_IO_time = 0;
     }
-    PCB(int id,string name,int priority,int a_time,char type,int cpu_time,int io_time)
+    PCB(int id, string name, int priority, int a_time, char type, int cpu_time, int io_time)
     {
-        process_id=id;
-        process_name=name;
-        process_priority=priority;
-        process_arival_time=a_time;
-        process_type=type;
-        process_cpu_time=cpu_time;
-        process_IO_time=io_time;
+        process_id = id;
+        process_name = name;
+        process_priority = priority;
+        process_arival_time = a_time;
+        process_type = type;
+        process_cpu_time = cpu_time;
+        process_IO_time = io_time;
     }
 };
 
@@ -66,74 +66,71 @@ public:
     int no_of_processes;
     SparkKernel()
     {
-        pcb_array=NULL;
-        no_of_processes=0;
+        pcb_array = NULL;
+        no_of_processes = 0;
     }
-    void newProcess(){
-        
+    void newProcess()
+    {
     }
 
-  
     void count_no_of_processes(string inputfile)
     {
         ifstream file(inputfile);
-        string line="";
-        getline(file,line);
-        while(getline(file,line))
+        string line = "";
+        getline(file, line);
+        while (getline(file, line))
         {
             no_of_processes++;
         }
         file.close();
     }
-    
+
     void file_handling(string inputfile)
     {
         count_no_of_processes(inputfile);
         pcb_array = new PCB[no_of_processes];
 
         ifstream file(inputfile);
-        string line="",str="";
+        string line = "", str = "";
         char chr;
-        int k=0,p=0;
-        getline(file,line);
-        while(!file.eof())
+        int k = 0, p = 0;
+        getline(file, line);
+        while (!file.eof())
         {
-            
-           file >> pcb_array[k].process_name;
-           file >> str;
-           p=stoi(str);
-           pcb_array[k].process_priority=p;
-           file >> str;
-           p=stoi(str);
-           pcb_array[k].process_arival_time=p;
-           file >> str;
-           pcb_array[k].process_type=str[0];
-           file >> str;
-           p=stoi(str);
-           pcb_array[k].process_cpu_time=p;
-           file >> str;
-           p=stoi(str);
-           pcb_array[k].process_IO_time=p;
-            k++;
 
-           
-        }  
-        
+            file >> pcb_array[k].process_name;
+            file >> str;
+            p = stoi(str);
+            pcb_array[k].process_priority = p;
+            file >> str;
+            p = stoi(str);
+            pcb_array[k].process_arival_time = p;
+            file >> str;
+            pcb_array[k].process_type = str[0];
+            file >> str;
+            p = stoi(str);
+            pcb_array[k].process_cpu_time = p;
+            file >> str;
+            p = stoi(str);
+            pcb_array[k].process_IO_time = p;
+            k++;
+        }
     }
 
     void show_pcb()
     {
-        for(int i=0;i<no_of_processes;i++)
+        cout << "PROCNAME\tPRIORITY\tARRIVAL-TIME\tPROC-TYPE\tCPU-TIME\tI/O-TIME" << endl;
+        for (int i = 0; i < no_of_processes; i++)
         {
-            cout<<pcb_array[i].process_name<<"\t";
-            cout<<pcb_array[i].process_priority<<"\t";
-            cout<<pcb_array[i].process_arival_time<<"\t";
-            cout<<pcb_array[i].process_type<<"\t";
-            cout<<pcb_array[i].process_cpu_time<<"\t";
-            cout<<pcb_array[i].process_IO_time<<"\n";
+            cout << pcb_array[i].process_name << "\t";
+            // For Row Alignment
+            if (pcb_array[i].process_name.length() < 8)
+                cout << "\t";
+            cout << pcb_array[i].process_priority << "\t\t";
+            cout << pcb_array[i].process_arival_time << "\t\t";
+            cout << pcb_array[i].process_type << "\t\t";
+            cout << pcb_array[i].process_cpu_time << "\t\t";
+            cout << pcb_array[i].process_IO_time << "\n";
         }
     }
-
-
-
 };
